b89ffb3d719e24bb29a9d57cf189f5962caafdd4
Robert Weiner <rsw@gnu.org>
2024-01-06 12:09:55 +0000

    Release 1.0.6 - Extensive doc updates and utiliy function additions

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # ert-results.el
 
 This package augments `ert-results-mode` (where test results are
-displayed) from the [Emacs Regression Test](https://www.gnu.org/software/emacs/manual/html_mono/ert.html) library, "ert.el", with two new commands:
+displayed) from the [Emacs Regression Test](https://www.gnu.org/software/emacs/manual/html_mono/ert.html) library, "ert.el", with four new commands:
 
-{f} - (ert-results-filter) - filters display of test results based on point
+{e} - `ert-results-edebug`  - edebugs the test at point in the results buffer or within a test definition
 
-{t} - (ert-results-toggle) - toggles display/hiding of all selected tests
+{f} - `ert-results-filter`  - filters display of test results based on point
+
+`{M-x ert-results-run RET}` - runs the test at point in the results buffer or within a test definition; no keybinding, but could be bound to {r} by the user if desired to replace the built-in ERT command.
+
+{t} - `ert-results-toggle`  - toggles display/hiding of all selected tests
 
 ## [Home Page](https://github.com/rswgnu/ert-results)
 
@@ -18,6 +22,8 @@ Use the Melpa Emacs Lisp archive to install this as a regular package:
 
 (require 'ert-results)
 ```
+
+or install it directly from the Github archive above as it is a single file code library.
 
 ## Usage
 
@@ -37,6 +43,10 @@ To use the package after installing it and requiring it:
 
 6. Press {f} on any of the statistics header lines, on the results summary
    line or on any test result to filter to results with associated statuses.
+
+7. On a test entry, press {r} to run any test, {e} to edebug a test or {d}
+   to run a test with `debug-on-error' enabled, which generates a backtrace
+   if any error is encountered during the run.
 
 ### Usage Details
 
@@ -60,13 +70,35 @@ results buffer, {f} does the following:
 	| Skipped:  | show skipped tests only                 |
 	| Total:    | show all tests                          |
 
+A press of {e}, the `ert-results-edebug` command, edebugs the test at
+point within the ERT results buffer, but also can be invoked with
+`{M-x ert-results-edebug RET}` within any ert test definition in any
+buffer.  It instruments the current version of the given test for
+electric debugging (edebugging) and then steps through it in the test
+source buffer with edebug.
+
+`{M-x ert-results-run RET}' works just like `ert-results-edebug' but
+instead of edebugging, it simply runs the current version of the test.
+You can bind it yourself to {r} to replace ert's
+`ert-results-rerun-test-at-point' command if you prefer a single
+command that you can use in both the results buffer and test
+definitions.
+
 This package also provides the following commands:
 
-    | Command               | Operation                                  |
-	|-----------------------|--------------------------------------------|
-	| `ert-results-hide`    |hide all test results                       |
-	| `ert-results-show`    |show all test results                       |
-	| `ert-results-display` |show all test results; with prefix arg, hide|
+    | Command               | Operation                                    |
+	|-----------------------|----------------------------------------------|
+	| `ert-results-hide`    | hide all test results                        |
+	| `ert-results-show`    | show all test results                        |
+	| `ert-results-display` | show all test results; with prefix arg, hide |
+
+And the following functions for programming:
+
+    | Function                      | Operation                               |
+	|-------------------------------|-----------------------------------------|
+    | `ert-results-all-test-bodies' | get a list of test bodies in the buffer |
+    | `ert-results-all-test-names'  | get a list of test names in the buffer  |
+    | `ert-results-all-test-tags'   | get a list of test tags in the buffer   |
 
 
 ## Rationale
--- ert-results.el
+++ ert-results.el
@@ -1,7 +1,7 @@
 ;;; ert-results.el --- Filter ERT test results display   -*- lexical-binding: t; -*-
 ;; Usage:        GNU Emacs Lisp Library
 ;; Keywords:     lisp, maint, tools
-;; Version:      1.0.5
+;; Version:      1.0.6
 ;;
 ;; Author:       Robert Weiner <rsw@gnu.org>
 ;;
@@ -9,7 +9,7 @@
 ;; URL:              https://github.com/rswgnu/ert-results
 ;;
 ;; Orig-Date:    28-Dec-23 at 14:52:30
-;; Last-Mod:      5-Jan-24 at 18:46:20 by Bob Weiner
+;; Last-Mod:      6-Jan-24 at 12:05:20 by Bob Weiner
 ;;
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -23,25 +23,23 @@
 ;;
 ;; See documentation for `ert-select-tests' for test SELECTOR types.
 ;;
-;; Within an interactive Emacs session, if `ert' is called with a regular
-;; expression selector argument that matches to multiple test names, all
-;; such tests are run, but only a summary line of output is displayed for
-;; all passing tests.  Thus, the names of the passing matched tests are
-;; not displayed.  Presently, within the *ert* output buffer, you must press
-;; {j} on each individual test summary result to display and jump to the
-;; test name and have its status spelled out.
+;; Within an interactive Emacs session, if `ert' is called with a
+;; regular expression selector argument that matches to multiple test
+;; names, all such tests are run, but only a summary line of output is
+;; displayed for all passing tests.  Thus, the names of the passing
+;; matched tests are not displayed.  Presently, within the *ert* output
+;; buffer, you must press {j} on each individual test summary result to
+;; display and jump to the test name and have its status spelled out,
+;; rather than abbreviated.  This library fixes these problems and
+;; extends the ert library with a number of useful commands.
 ;;
-;; This library fixes the above with the `ert-results-toggle' command bound
-;; to {t} within the ERT results buffer.  With each press, it toggles
-;; between showing/hiding all test results.  After hiding the results,
-;; point is left on the summary item associated with the test that had
-;; been at point, if any.
+;; The first command, `ert-results-toggle', is bound to {t} within the
+;; ERT results buffer.  With each press, it toggles between showing and
+;; hiding all test results.  After hiding the results, point is left on
+;; the summary item associated with the test that had been at point, if
+;; any.
 ;;
-;; The second command, `ert-results-debug', is bound to {e} within the
-;; ERT results buffer.  It reruns the test at point using the electric
-;; debugger, letting you single-step through your test definitions.
-;;
-;; The third command, `ert-results-filter', is bound to {f} within
+;; The second command, `ert-results-filter', is bound to {f} within
 ;; the ERT results buffer.  It filters the results to just those entries
 ;; whose status matches that of the current entry.  Point may be on a
 ;; results entry or on a result character in the results summary.
@@ -54,7 +52,25 @@
 ;;   Skipped:  - show skipped tests only
 ;;   Total:    - show all tests
 ;;
-;; This library also provides the following commands:
+;; The third command, `ert-results-edebug', is bound to {e} within the
+;; ERT results buffer, but also can be invoked by name within any ert
+;; test definition in any buffer.  It instruments the current version of
+;; the given test for electric debugging (edebugging) and then steps
+;; through it in the test source buffer with edebug.
+;;
+;; The fourth command, `ert-results-run', is not bound to a key.  It
+;; works just like `ert-results-edebug' but instead of edebugging, it
+;; simply runs the current version of the test.  You can bind it
+;; yourself to {r} to replace ert's `ert-results-rerun-test-at-point'
+;; command if you prefer a single command that you can use in both the
+;; results buffer and test definitions.
+;;
+;; All key bindings for this library are made only if the "ert" library
+;; or the user has not already bound them in `ert-results-mode'.
+;;
+;; ------------
+;;
+;; This library also provides the following commands for the results buffer:
 ;;   `ert-results-hide'     - hide all test results
 ;;   `ert-results-show'     - show all test results
 ;;   `ert-results-display'  - show all test results; with prefix arg, hide
@@ -64,6 +80,8 @@
 ;;   `ert-results-all-test-bodies' - get a list of test bodies in the buffer
 ;;   `ert-results-all-test-names'  - get a list of test names in the buffer
 ;;   `ert-results-all-test-tags'   - get a list of test tags in the buffer
+;;
+;; ------------
 ;;
 ;; To use:
 ;;
@@ -87,6 +105,7 @@
 ;;; Public declarations
 ;;; ************************************************************************
 
+(defvar ert-results--buffer)
 (defvar ert--results-ewoc)
 (defvar ert--results-progress-bar-button-begin)
 
@@ -100,6 +119,8 @@ With optional prefix arg HIDE-FLAG non-nil, hide these instead.
 With optional STATUS-TO-SHOW symbol (one of :passed :failed
 :aborted :skipped), show/keep only entries with that status."
   (interactive "P" ert-results-mode)
+  (unless (derived-mode-p 'ert-results-mode)
+    (error "(ert-results-display): Use only in an ERT results buffer"))
   (when (boundp 'ert--results-ewoc)
     (let ((ewoc ert--results-ewoc)
           (progress-bar-begin ert--results-progress-bar-button-begin)
@@ -139,20 +160,18 @@ With optional STATUS-TO-SHOW symbol (one of :passed :failed
 	(goto-char (+ progress-bar-begin result-num))))))
 
 (defun ert-results-edebug ()
-  "Re-run the test named at point with the edebugger."
-  (interactive nil ert-results-mode)
-  (let ((test-symbol (ert-test-at-point)))
-    (unless test-symbol
-      (user-error "No test at point"))
-    (ert-find-test-other-window test-symbol)
-    (ert-results--run-test-at-definition test-symbol t)))
+  "Eval and edebug any ert test at point and return t, else return nil.
+Point may be within an ert test def or in an ert results buffer and on a
+test or test summary item."
+  (interactive)
+  (ert-results--run-test-at-definition t))
 
 (defun ert-results-filter (&optional status-symbol)
   "Filter ert results entries to those matching optional STATUS-SYMBOL.
 If STATUS-SYMBOL is null, use the entry type at point (context-sensitive).
 
 If STATUS-SYMBOL is :passed :failed :aborted or :skipped (on any of the
-Passed: Failed: Aborted. or Skipped: header lines), only results with the
+Passed: Failed: Aborted or Skipped: header lines), only results with the
 associated status are shown.
 
 If STATUS-SYMBOL is :selector (on the header Selector: line), toggle
@@ -161,7 +180,7 @@ showing/hiding all test results.
 If STATUS-SYMBOL is :total (on the header Total: line), toggle
 showing/hiding all test results.
 
-If STATUS-SYMBOL is :aborted (on the header Aborted. line), do nothing.
+If STATUS-SYMBOL is :aborted (on the header Aborted line), do nothing.
 
 Return STATUS-SYMBOL."
   (interactive nil ert-results-mode)
@@ -215,6 +234,13 @@ test it was on, if any."
   (interactive nil ert-results-mode)
   (ert-results-display t))
 
+(defun ert-results-run ()
+  "Eval and run any ert test at point and return t, else return nil.
+Point may be within an ert test def or in an ert results buffer and on a
+test or test summary item."
+  (interactive)
+  (ert-results--run-test-at-definition))
+
 (defun ert-results-show ()
   "Show all test names and results in the *ert* buffer."
   (interactive nil ert-results-mode)
@@ -249,6 +275,18 @@ to the displayed test it was on, if any."
 ;;; Private functions
 ;;; ************************************************************************
 
+(defun ert-results--all-test-numbers-and-names ()
+  "Return a list of (test-num . test-symbol) items from the ert results buffer.
+Test numbers start at 0."
+  (let ((counter -1)
+	(results-buffer (get-buffer ert-results--buffer)))
+    (when results-buffer
+      (with-current-buffer results-buffer
+	(ert-results--map-tests
+	 (lambda (test)
+	   (setq counter (1+ counter))
+	   (cons counter (ert-test-name test))))))))
+
 (defun ert-results--char-str-to-status-symbol (char-str)
   "Given single CHAR-STR, convert it to a results status symbol for return."
   (pcase (downcase char-str)
@@ -260,42 +298,91 @@ to the displayed test it was on, if any."
     ("q" :quit)
     ("s" :skipped)))
 
+(defun ert-results--goto-test-number (test-num)
+  "Jump to the ert results buffer summary item given by TEST-NUM.
+Trigger an error if cannot jump there."
+  (if (not (wholenump test-num))
+      (error "(ert-results--goto-test-number): `test-num' must be a whole number, not '%s'"
+	     test-num)
+    (let ((results-buffer (get-buffer ert-results--buffer)))
+      (if (not results-buffer)
+	  (error "(ert-results--goto-test-number): No results \"%s\" buffer found"
+		 ert-results--buffer)
+	(switch-to-buffer results-buffer)
+	(goto-char (+ ert--results-progress-bar-button-begin test-num))))))
+
+(defun ert-results--in-summary-bar-p (&optional pos)
+  "Return t if optional POS (default = point) is in the ert results status bar.
+Return nil otherwise."
+  (when (derived-mode-p 'ert-results-mode)
+    (unless pos
+      (setq pos (point)))
+    (let ((progress-bar-begin ert--results-progress-bar-button-begin))
+      (and (<= progress-bar-begin pos)
+	   (< pos (button-end (button-at progress-bar-begin)))))))
+
 (defun ert-results--map-tests (func)
   "In an ert results buffer, return result of applying FUNC over its tests.
 For example, to get all test names: (ert-results-map-tests #'ert-test-name)."
-  (if (not (derived-mode-p 'ert-results-mode))
-      (error "(ert-results-map-tests): Use only in an ERT results buffer")
-    (let ((ewoc ert--results-ewoc)
-	  node
-	  entry
-	  result
-	  results)
-      (ewoc--set-buffer-bind-dll ewoc
-	(setq node (ewoc-nth ewoc 0))
-	(while (and node (setq entry (ewoc-data node))
-		    (ert--ewoc-entry-p entry))
-	  (setq result (funcall func (ert--ewoc-entry-test entry))
-		results (cons result results)
-		node (ewoc--node-next dll node)))
-	results))))
+  (unless (derived-mode-p 'ert-results-mode)
+    (error "(ert-results-map-tests): Use only in an ERT results buffer"))
+  (let ((ewoc ert--results-ewoc)
+	node
+	entry
+	result
+	results)
+    (ewoc--set-buffer-bind-dll ewoc
+      (setq node (ewoc-nth ewoc 0))
+      (while (and node (setq entry (ewoc-data node))
+		  (ert--ewoc-entry-p entry))
+	(setq result (funcall func (ert--ewoc-entry-test entry))
+	      results (cons result results)
+	      node (ewoc--node-next dll node)))
+      results)))
 
-(defun ert-results--run-test-at-definition (test-name &optional edebug-it)
-  "Eval and run the ert TEST-NAME defined at point.
-Assume point is on the text of the first line of an ert test def,
+(defun ert-results--run-test-at-definition (&optional edebug-it)
+  "Eval and run any ert test at point and return t, else return nil.
+Point may be within an ert test def or in an ert results buffer and on a
+test or test summary item.
+
 With optional EDEBUG-IT non-nil, edebug the test when it is run."
-  (let ((test-sym (if (symbolp test-name) test-name (intern-soft test-name))))
-    ;; Ensure run the latest version of the test, either with the
-    ;; edebugger if already instrumented for it; otherwise, with the
-    ;; normal evaluator.
-    (if (and test-sym edebug-it)
-	(edebug-defun)
-      (eval-defun nil)
-      (setq test-sym (intern-soft test-name))
-      (when (and test-sym edebug-it)
-	(edebug-defun)))
-    (setq test-sym (intern-soft test-name))
-    (when (and test-sym (ert-test-boundp test-sym))
-      (ert test-sym))))
+  (save-excursion
+    (save-window-excursion
+      (let ((in-summary-bar (ert-results--in-summary-bar-p)))
+	(when (derived-mode-p 'ert-results-mode)
+	  ;; Next line errors if not found
+	  (ert-results-find-test-at-point-other-window))
+	;; Ensure run the latest version of the test, either with the
+	;; edebugger if `edebug-it' is non-nil; otherwise, with the
+	;; normal evaluator.
+	(let ((test-sym (if edebug-it
+			    (edebug-defun)
+			  (eval-defun nil))))
+	  (when (and test-sym (ert-test-boundp test-sym))
+	    (let ((test-num (ert-results--test-number test-sym)))
+	      (if test-num
+		  ;; test is already in results buffer
+		  (progn (ert-results--goto-test-number test-num)
+			 (if in-summary-bar
+			     (ert-results-rerun-test-at-point)
+			   (ert-results-jump-between-summary-and-result)
+			   (ert-results-rerun-test-at-point)
+			   (goto-char (+ (point) 2))))
+		(ert test-sym))
+	      t)))))))
+
+(defun ert-results--test-number (test-symbol)
+  "Return the index number of the Ert results buffer TEST-SYMBOL.
+Indexes start from 0."
+  (when (and test-symbol (symbolp test-symbol) (ert-test-boundp test-symbol))
+    (car (rassq test-symbol (ert-results--all-test-numbers-and-names)))))
+
+;;; ************************************************************************
+;;; Private variables
+;;; ************************************************************************
+
+(defvar ert-results--buffer "*ert*"
+  "Buffer name for standard ERT results.")
 
 ;;; ************************************************************************
 ;;; Key bindings
